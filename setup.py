1e3ecdaf1cc5280db84195dc9947d1f37d71bd4e
umut <umutcayli@outlook.com>
2020-03-01 22:22:45 +0300

    Site için HTML dosyasını değiştirdim.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+# Source: https://marthall.github.io/blog/how-to-package-a-python-app/
+setup(
+name='My_Head_First_Python__vsearch_module',
+version='0.1',
+scripts=['vsearch']
+)
